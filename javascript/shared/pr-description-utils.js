cdc4f971006984f04e971f3e20301dab130f17a1
dornby <hugo@getaround.com>
2026-01-26 11:17:00 +0100

    Adapt to new commit msg

--- javascript/shared/pr-description-utils.js
+++ javascript/shared/pr-description-utils.js
@@ -54,7 +54,7 @@ function replaceCommitsWith(commits, textAreaContent) {
 
   const newCommitsContent = commits
     .map(c => c.commit.message)
-    .filter(msg => !msg.startsWith("Translation update from Lyric"))
+    .filter(msg => !msg.startsWith("Translation update from Lyriq"))
     .filter(msg => !msg.startsWith("[Copilot]"))
     .filter(msg => shouldAddCommit(commitsContent, msg))
     .map(msg => `\n### ${msg}`)
